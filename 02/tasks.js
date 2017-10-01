22fdc858278429262b15f85088b0579e981b78fa
termit1337 <askask@mail.ru>
2017-10-01 18:08:52 +0300

    getUniq task

--- 02/tasks.js
+++ 02/tasks.js
@@ -77,7 +77,14 @@ function anagram(first, second) {
  * @return {Array<number>} массив уникальных значений, отсортированный по возрастанию
  */
 function getUnique(arr) {
-  return [];
+  let obj = {};
+    
+  for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+    obj[str] = true;
+    }
+	
+  return Object.keys(obj);
 }
 
 /**
